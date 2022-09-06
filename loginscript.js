985741952cb2a88a3acf94f75e5435284239a5ee
Aditi101049 <103937825+Aditi101049@users.noreply.github.com>
2022-09-06 10:16:56 +0530

    Add files via upload

---
+++ loginscript.js
@@ -0,0 +1,14 @@
+
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "g5" && password == "g5"){
+    alert ("Login successfully");
+    window.location = "file:///C:/Users/Digambar/Desktop/coffee/home%20page.html"; // Redirecting to other page.
+    return false;
+    }
+    
+    }
+    
+
+     
